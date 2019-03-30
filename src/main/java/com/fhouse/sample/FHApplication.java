6f06806385d2d64c649ed519e145e6340e874f1b
benan.aktas <benan.aktas@n11.com>
2019-03-30 16:26:16 +0300

    exceptions

--- src/main/java/com/fhouse/sample/FHApplication.java
+++ src/main/java/com/fhouse/sample/FHApplication.java
@@ -1,26 +1,65 @@
 package com.fhouse.sample;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.TimeZone;
 
+@Slf4j
 @SpringBootApplication
 @EntityScan(basePackageClasses = {
 		FHApplication.class,
 		Jsr310JpaConverters.class
 })
-public class FHApplication {
+@ComponentScan({"com.fhouse"})
+public class FHApplication  extends SpringBootServletInitializer {
 
 	@PostConstruct
 	void init() {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(FHApplication.class, args);
+	public static void main(String[] args) throws UnknownHostException {
+
+		SpringApplication app = new SpringApplication(FHApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+		Environment env = app.run(args).getEnvironment();
+
+		log.info("Access URLs:\n----------------------------------------------------------\n\t" +
+						"Local: \t\thttp://127.0.0.1:{}\n\t" +
+						"External: \thttp://{}:{}\n----------------------------------------------------------",
+				env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"));
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application;
+	}
+
+	@Bean(name = "messageSource")
+	public ResourceBundleMessageSource getMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("i18n/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setUseCodeAsDefaultMessage(true);
+		return messageSource;
+	}
+
 }
--- src/main/java/com/fhouse/sample/exception/ResourceNotFoundException.java
+++ src/main/java/com/fhouse/sample/exception/ResourceNotFoundException.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Data
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
+
     private String resourceName;
     private String fieldName;
     private Object fieldValue;
